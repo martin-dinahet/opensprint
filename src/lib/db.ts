606cdfca3d66268d4a9c2080a71729da632c0690
Martin <martin.dinahet@proton.me>
2026-04-19 20:34:21 +0200

    feature: add drizzle db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,6 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma/client";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = new PrismaClient({
-  adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-  }),
-});
+const url = process.env.DATABASE_URL;
+if (!url) throw new Error("Can't access database URL");
+
+export const db = drizzle(url);
