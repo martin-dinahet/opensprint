27bac7ca043050440104b7fed838fc21faaedc55
Martin <martin.dinahet@proton.me>
2026-04-22 19:54:29 +0200

    chore: format

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,8 @@ const url = process.env.DATABASE_URL;
 if (!url) throw new Error("DATABASE_URL environment variable not found.");
 
 export default defineConfig({
-	out: "./drizzle",
-	schema: "./src/server/db/schema.ts",
-	dialect: "postgresql",
-	dbCredentials: { url },
+  out: "./drizzle",
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: { url },
 });
