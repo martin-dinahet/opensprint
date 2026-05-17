7237ebfce492f0977efbac516746c4a52f9c21df
Martin <martin.dinahet@proton.me>
2026-05-17 02:08:45 +0200

    hotfix: updated auth config

--- src/server/lib/auth.ts
+++ src/server/lib/auth.ts
@@ -10,6 +10,6 @@ const db = drizzle(new Pool({ connectionString: process.env.DATABASE_URL }), {
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", schema }),
-  baseURL: "http://localhost:3000/",
+  baseURL: `${process.env.BETTER_AUTH_URL}`,
   emailAndPassword: { enabled: true },
 });
